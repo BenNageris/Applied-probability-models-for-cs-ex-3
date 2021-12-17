98a76c959d3e9edb7d8e94235e994ec1534c51ca
YanivZimmer <zimmer.yaniv@gmail.com>
2021-12-17 15:39:54 +0200

    calculate e_zi and w_ti

--- code/E.py
+++ code/E.py
@@ -15,3 +15,14 @@ def max_z(p_ti:list[dict],n_tk:list[dict],alpha:list,t:int)
         max_z=max(max_z,Z(p_ti=p_ti,n_tk=n_tk,alpha=alpha,t=t,i=i))
     return max_z
 
+def calc_ez(z:float,m:float,k:int)->float:
+    if z[i]-m<-k:
+        return 0
+    return math.pow(math.e,z-m)     
+
+def wti(z:list[float],i:int,m:float,k:int):
+    if z[i]-m<-k:
+        return 0
+    numerator= calc_ez(z[i],m,k,i)
+    denominator=sum([calc_ez(z[j],m,k) for j in range(len(z))])
+    return numerator/denominator   
